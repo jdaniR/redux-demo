420d260906a782b868f4d56d3a7143fa13f7a6c7
j.dani <janki@innovactdigital.com>
2022-01-07 11:22:31 +0000

    Task 1 : Install Redux Library Task 2: Create a js file and add Javascript function and action.

---
+++ index.js
@@ -0,0 +1,13 @@
+const BUY_CAKE = "BUY_CAKE";
+
+{
+  type: BUY_CAKE,
+  info: 'FIRST Redux Action'
+}
+
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: 'FIRST Redux Action'
+      }
+}
