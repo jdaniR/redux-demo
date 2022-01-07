9b4c17cef461f97525fcb6ec46260570e5e2982d
j.dani <janki@innovactdigital.com>
2022-01-07 11:33:35 +0000

    Task-6: created js function, action and reducer for the cake shop example.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function buyCake(){
         info: 'FIRST Redux Action'
       }
 }
+
+// (previousState, action) => newState
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer =(state = initialState, action) =>{
+ switch (action.type) {
+     case BUY_CAKE: return {
+         ...state,
+         numOfCakes: state.numOfCakes-1
+     }
+     default:
+         return state
+ }
+}
