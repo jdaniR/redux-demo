40fc56790b3ee53958e13335c2163c70ba98b44b
j.dani <janki@innovactdigital.com>
2022-01-07 12:15:41 +0000

    Task-9.2: Separate Cake Reducer and Ice cream reducer. Task-10: Use combineReducers() form Redux lib.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -20,29 +21,66 @@ function buyIcecream() {
 
 // (previousState, action) => newState
 
-const initialState = {
+// const initialState = {
+//   numOfCakes: 10,
+//   numberOfIcecreams: 20,
+// };
+
+const initialCakeState = {
   numOfCakes: 10,
-  numberOfIcecreams: 20,
 };
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1,
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state,
+//         numberOfIcecreams: state.numberOfIcecreams - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
-        numberOfIcecreams: state.numberOfIcecreams - 1,
+        numOfIceCreams: state.numOfIceCreams - 1,
       };
     default:
       return state;
   }
 };
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
 console.log("Initial state", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("updateState", store.getState())
